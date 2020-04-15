b9ee0f6c75e44f6a9f522d8c192d5f70adf2b401
Ankit Shukla <4nkit5hukla@gmail.com>
2020-04-15 14:24:55 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,12 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404 Page Not found" />
+    <div className="container-fluid text-center">
+      <h1 className="font-10x my-5">404</h1>
+      <h2 className="font-4x my-5">PAGE NOT FOUND</h2>
+      <p className="mb-4">You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
